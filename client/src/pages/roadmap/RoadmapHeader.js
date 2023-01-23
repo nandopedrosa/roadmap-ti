c41fe54ab1cc914dc37c3c06a040251abe941fba
Fernando Pedrosa <fpedrosa@gmail.com>
2023-01-23 15:56:10 -0300

    Add warning to roadmap

--- client/src/pages/roadmap/RoadmapHeader.js
+++ client/src/pages/roadmap/RoadmapHeader.js
@@ -6,13 +6,14 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 function RoadmapHeader({ nome, descricao }) {
     return (
         <div style={{ paddingTop: 45, paddingBottom: 30 }}>
-            <div className="container text-center" style={{ maxWidth: "42rem" }}>
-                <h1 className="display-4">{nome}</h1>
-                <h1>
+            <div className="container" style={{ maxWidth: "42rem" }}>
+                <h1 className="text-center display-4">{nome}</h1>
+                <h1 className="text-center">
                     <p className="lead text-muted">
                         <div dangerouslySetInnerHTML={{ __html: descricao }}></div>
                     </p>
                 </h1>
+                <p className="fst-italic"><i className="bi bi-exclamation-circle"></i> Este roteiro não contém todos os assuntos possíveis da disciplina (foco no essencial).</p>
             </div>
         </div>
     );
